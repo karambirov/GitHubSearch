6110c9b486667abd1bb9e3f4272f993da80ff082
Karambirov <eugenekarambirov@gmail.com>
2018-02-28 18:42:42 +0300

    Create TabBarController

---
+++ GitHubSearch/Controllers/TabBar/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubSearch
+//
+//  Created by Eugene Karambirov on 28/02/2018.
+//  Copyright © 2018 Eugene Karambirov. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
