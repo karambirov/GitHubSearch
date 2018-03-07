4727c2002e803e4b626ab522946c805215f523fb
Karambirov <eugenekarambirov@gmail.com>
2018-03-07 14:05:51 +0300

    Set TabBar as root view controller

--- GitHubSearch/AppDelegate.swift
+++ GitHubSearch/AppDelegate.swift
@@ -17,8 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
-//        window?.rootViewController = TabBarController.instantiateFromNib()
-        window?.rootViewController = SearchViewController.instantiateFromNib().embedInNavigationController()
+        window?.rootViewController = TabBarController()
         window?.makeKeyAndVisible()
         
         return true
