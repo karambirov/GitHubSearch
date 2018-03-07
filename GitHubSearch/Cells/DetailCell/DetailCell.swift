c88ef6df2da960fd4409ed48ad6b1325cb698589
Karambirov <eugenekarambirov@gmail.com>
2018-03-07 09:42:00 +0300

    Create DetailCell

---
+++ GitHubSearch/Cells/DetailCell/DetailCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailCell.swift
+//  GitHubSearch
+//
+//  Created by Eugene Karambirov on 07/03/2018.
+//  Copyright © 2018 Eugene Karambirov. All rights reserved.
+//
+
+import UIKit
+
+class DetailCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
