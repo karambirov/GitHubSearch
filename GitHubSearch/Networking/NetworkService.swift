8cd9693aaec18a0f5e41d0fc8d985c6cc8ce1264
Karambirov <eugenekarambirov@gmail.com>
2018-03-12 19:37:02 +0300

    Implement base setup of Moya

--- GitHubSearch/Networking/NetworkService.swift
+++ GitHubSearch/Networking/NetworkService.swift
@@ -7,8 +7,56 @@
 //
 
 import Foundation
+import Moya
 
-final class NetworkService {
+public enum NetworkService {
+    case repoSearch(query: String)
+}
+
+// MARK: - TargetType Protocol Implementation
+extension NetworkService: TargetType {
+    
+    public var baseURL: URL {
+        return URL(string: "https://api.github.com")!
+    }
+    
+    public var path: String {
+        switch self {
+        case .repoSearch(_):
+            return "/search/repositories"
+        }
+    }
+    
+    public var method: Moya.Method {
+        return .get
+    }
+    
+    public var parameters: [String: Any]? {
+        switch self {
+        case .repoSearch(let query):
+            return ["q": query.URLEscapedString]
+        }
+    }
+    
+    public var parameterEncoding: ParameterEncoding {
+        return URLEncoding.default
+    }
+    
+    public var sampleData: Data {
+        switch self {
+        case .repoSearch(_):
+            return "Half measures are as bad as nothing at all.".utf8Encoded
+        }
+    }
+    
+    public var task: Task {
+        return .requestPlain
+    }
+    
+    public var headers: [String : String]? {
+        return ["Content-type": "application/json"]
+    }
+    
     
 }
 
