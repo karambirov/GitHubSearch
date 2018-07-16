e477caa65d55577b50b501200a9e4c4103e74c5e
Karambirov <eugenekarambirov@gmail.com>
2018-07-16 13:59:35 +0300

    Create models for Repository and User

---
+++ GitHubSearch/Models/User+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  User+CoreDataClass.swift
+//  GitHubSearch
+//
+//  Created by Eugene Karambirov on 16/07/2018.
+//  Copyright © 2018 Eugene Karambirov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
---
+++ GitHubSearch/Models/User+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  User+CoreDataProperties.swift
+//  GitHubSearch
+//
+//  Created by Eugene Karambirov on 16/07/2018.
+//  Copyright © 2018 Eugene Karambirov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension User {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<User> {
+        return NSFetchRequest<User>(entityName: "User")
+    }
+
+    @NSManaged public var login: String?
+    @NSManaged public var email: String?
+    @NSManaged public var avatarURL: String?
+
+}
