34b16d1f04d8bcf105477a2b1f4196ff1fcbccf7
Karambirov <eugenekarambirov@gmail.com>
2018-07-21 13:03:52 +0300

    Create loadData implementation

--- GitHubSearch/Services/NetworkingService.swift
+++ GitHubSearch/Services/NetworkingService.swift
@@ -12,10 +12,35 @@ final class NetworkingService<Resource: ResourceType>: NetworkingType {
 
     init() {}
 
-    func request(resource: Resource, session: NetworkingSession,
+    func request(resource: Resource,
+                 session: NetworkingSession = URLSession.shared,
                  completion: @escaping (NetworkingResult<Response>) -> Void) -> URLSessionDataTask {
 
         let request = URLRequest(resource: resource)
+
+        return session.loadData(with: request) { data, response, error in
+            guard error == nil else {
+                completion(.error(.error(error)))
+                return
+            }
+
+            guard let data = data else {
+                completion(.error(.emptyResult))
+                return
+            }
+
+            guard let response = response as? HTTPURLResponse else {
+                completion(.error(.noHttpResponse))
+                return
+            }
+
+            guard 200..<300 ~= response.statusCode else {
+                completion(.error(.requestFailed(data)))
+                return
+            }
+
+            completion(.success(Response(urlRequest: request, data: data)))
+        }
     }
 
 }
