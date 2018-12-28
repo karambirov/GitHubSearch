ed04d85c248015ec8e6b58f33eadf04fc2f90449
Karambirov <eugenekarambirov@gmail.com>
2018-12-28 10:24:07 +0300

    Create ViewController

---
+++ GitHubSearch/Modules/Views/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  GitHubSearch
+//
+//  Created by Eugene Karambirov on 28/12/2018.
+//  Copyright © 2018 Eugene Karambirov. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+}
