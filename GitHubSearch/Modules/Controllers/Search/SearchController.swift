14ab3c2813996a029c012dfe7f853ef66f43afb9
Karambirov <eugenekarambirov@gmail.com>
2018-12-28 10:13:39 +0300

    Set SearchController as root

--- GitHubSearch/Modules/Controllers/Search/SearchController.swift
+++ GitHubSearch/Modules/Controllers/Search/SearchController.swift
@@ -12,7 +12,7 @@ final class SearchController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
     }
 
 }
