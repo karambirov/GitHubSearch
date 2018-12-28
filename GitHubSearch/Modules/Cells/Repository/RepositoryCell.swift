49d04be6fd85c0440e28f94eb75931723588e24c
Karambirov <eugenekarambirov@gmail.com>
2018-12-28 10:22:17 +0300

    Create TableViewController

--- GitHubSearch/Modules/Cells/Repository/RepositoryCell.swift
+++ GitHubSearch/Modules/Cells/Repository/RepositoryCell.swift
@@ -20,5 +20,5 @@ class RepositoryCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    
+
 }
---
+++ GitHubSearch/Modules/Views/TableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewController.swift
+//  GitHubSearch
+//
+//  Created by Eugene Karambirov on 28/12/2018.
+//  Copyright © 2018 Eugene Karambirov. All rights reserved.
+//
+
+import UIKit
+
+final class TableViewController: ViewController, UIScrollViewDelegate {
+
+}
