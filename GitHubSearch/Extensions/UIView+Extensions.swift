a9dc1dbec9c8b8eacc8a5fd6e7d6cd04ab9eead4
Karambirov <eugenekarambirov@gmail.com>
2018-12-28 12:03:45 +0300

    Create base classes TableView and View

---
+++ GitHubSearch/Extensions/UIView+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+Extensions.swift
+//  GitHubSearch
+//
+//  Created by Eugene Karambirov on 28/12/2018.
+//  Copyright © 2018 Eugene Karambirov. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+
+    var inset: CGFloat {
+        return Configs.BaseDimensions.inset
+    }
+
+}
