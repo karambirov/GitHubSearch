d412fdf0dc6f258d943bc904697ec0234792a1ff
Karambirov <eugenekarambirov@gmail.com>
2019-01-31 19:38:16 +0300

    Create TabBarController

---
+++ GitHubSearch/Modules/Controllers/TabBar/TabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarController.swift
+//  GitHubSearch
+//
+//  Created by Eugene Karambirov on 31/01/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+}
