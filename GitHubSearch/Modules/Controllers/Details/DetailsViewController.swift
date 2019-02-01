2e1bc1eb8df87f36ea693ba369461b894d248081
Karambirov <eugenekarambirov@gmail.com>
2019-02-01 15:46:51 +0300

    Add Transition protocol

--- GitHubSearch/Modules/Controllers/Details/DetailsViewController.swift
+++ GitHubSearch/Modules/Controllers/Details/DetailsViewController.swift
@@ -10,4 +10,8 @@ import UIKit
 
 final class DetailsViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.lightGray
+    }
 }
