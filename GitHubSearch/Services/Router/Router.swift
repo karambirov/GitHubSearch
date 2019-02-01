b02705349055bd9747fb04d7a4b12c2f3cf9196f
Karambirov <eugenekarambirov@gmail.com>
2019-02-01 16:32:09 +0300

    Create Router

--- GitHubSearch/Services/Router/Router.swift
+++ GitHubSearch/Services/Router/Router.swift
@@ -8,6 +8,43 @@
 
 import UIKit
 
-class Router {
+protocol Closable: AnyObject {
+    func close()
+}
+
+protocol RouterProtocol {
+    associatedtype ViewController: UIViewController
+
+    /// Should be declared as weak.
+    var viewController: ViewController? { get }
+
+    func open(_ viewController: UIViewController, transition: Transition)
+}
+
+class Router<U>: RouterProtocol, Closable where U: UIViewController {
+
+    typealias ViewController = U
+
+    weak var viewController: ViewController?
+    var openTransition: Transition?
+
+    func open(_ viewController: UIViewController, transition: Transition) {
+        transition.viewController = self.viewController
+        transition.open(viewController)
+    }
+
+    func close() {
+        guard let openTransition = openTransition else {
+            assertionFailure("You should specify an open transition in order to close a module.")
+            return
+        }
+
+        guard let viewController = viewController else {
+            assertionFailure("Nothing to close.")
+            return
+        }
+
+        openTransition.close(viewController)
+    }
 
 }
