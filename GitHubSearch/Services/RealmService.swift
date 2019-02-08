14c5ec4e198f6b31d1ca320069e14acfbaeffd93
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-02-08 09:45:02 +0300

    Create RealmService

---
+++ GitHubSearch/Services/RealmService.swift
@@ -0,0 +1,14 @@
+//
+//  RealmService.swift
+//  GitHubSearch
+//
+//  Created by Eugene Karambirov on 08/02/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+import Foundation
+import Realm
+
+final class RealmService {
+
+}
