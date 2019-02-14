0ab8ad15c5f82b66a974c50e5a7cedc5cb91eb41
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-02-14 14:55:52 +0300

    Delete unnecessary models

--- GitHubSearch/Models/Realm/OwnerRealm.swift
+++
@@ -1,9 +0,0 @@
-//
-//  OwnerRealm.swift
-//  GitHubSearch
-//
-//  Created by Eugene Karambirov on 08/02/2019.
-//  Copyright © 2019 Eugene Karambirov. All rights reserved.
-//
-
-import Foundation
--- GitHubSearch/Models/Realm/RepositoryRealm.swift
+++
@@ -1,9 +0,0 @@
-//
-//  RepositoryRealm.swift
-//  GitHubSearch
-//
-//  Created by Eugene Karambirov on 08/02/2019.
-//  Copyright © 2019 Eugene Karambirov. All rights reserved.
-//
-
-import Foundation
