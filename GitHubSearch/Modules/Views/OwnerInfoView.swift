8aecae89c36d0afb6ba0daf20ebac94ef98e3220
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-02-15 10:55:15 +0300

    Create OwnerInfoView.swift

---
+++ GitHubSearch/Modules/Views/OwnerInfoView.swift
@@ -0,0 +1,9 @@
+//
+//  OwnerInfoView.swift
+//  GitHubSearch
+//
+//  Created by Eugene Karambirov on 15/02/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+import Foundation
