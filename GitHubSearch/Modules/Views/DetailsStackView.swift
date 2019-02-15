692d89f22700e2f55125424a505b887f885cdb82
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-02-15 12:04:33 +0300

    Create DetailsStackView.swift

---
+++ GitHubSearch/Modules/Views/DetailsStackView.swift
@@ -0,0 +1,16 @@
+//
+//  DetailsStackView.swift
+//  GitHubSearch
+//
+//  Created by Eugene Karambirov on 15/02/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+final class DetailsStackView: UIStackView {
+
+    
+
+}
