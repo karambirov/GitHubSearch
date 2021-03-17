0e367e75eb0107d34eeb20d9e5f91c9371c24a8a
Eugene Karambirov <eugenekarambirov@gmail.com>
2021-03-17 16:11:34 +0300

    Add stack view layout

--- App/Sources/Core/Services/Layout/Stack.swift
+++ App/Sources/Core/Services/Layout/Stack.swift
@@ -25,20 +25,43 @@ extension UIView {
 		var constraints: Constraints = []
 
 		for view in views {
-			translatesAutoresizingMaskIntoConstraints = false
+			view.translatesAutoresizingMaskIntoConstraints = false
 			addSubview(view)
 
-//			switch axis {
-//			case .vertical:
-//				constraints.append(view.top)
-//
-//			case .horizontal:
-//
-//			@unknown default:
-//				break
-//			}
-//		}
+			switch axis {
+			case .vertical:
+				constraints.append(view.top(to: previous ?? self, previous?.bottomAnchor ?? topAnchor, offset: offset))
+				constraints.append(view.leftToSuperview())
+				constraints.append(view.rightToSuperview())
 
+				if let lastView = views.last, view == lastView {
+					constraints.append(view.bottomToSuperview())
+				}
+
+			case .horizontal:
+				constraints.append(view.topToSuperview())
+				constraints.append(view.bottomToSuperview())
+				constraints.append(view.left(to: previous ?? self, previous?.rightAnchor ?? leftAnchor, offset: offset))
+
+				if let lastView = views.last, view == lastView {
+					constraints.append(view.rightToSuperview())
+				}
+
+			@unknown default:
+				fatalError()
+			}
+
+			if let width = width {
+				constraints.append(view.width(width))
+			}
+
+			if let height = height {
+				constraints.append(view.height(height))
+			}
+
+			offset = spacing
+			previous = view
+		}
 		return constraints
 	}
 }
